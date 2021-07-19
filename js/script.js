ee6fd89c58b6ef09a1b47482b4e8d77d352b026b
FranciscoMi <franlope@gmail.com>
2021-07-19 17:50:22 +0200

    añadida funcion calcform

--- js/script.js
+++ js/script.js
@@ -8,19 +8,37 @@
 
 function calcform(formCalc) {
   //Primero comprobamos que se haya escrito al menos, un usuario
-  console.log(formCalc)
   let msg = document.getElementById("form_msg")
   msg.innerText = ""
-  const usuario = parseInt(document.calc_form.form_People.value)
-  if (!usuario > 0) {
-    msg.innerText = "No pueden ser 0"
+  const usuario = document.calc_form.form_People.value
+  if (usuario <= 0) {
+    msg.innerText = "Usuarios Incorrecto"
     return 0
   }
-  //Luego comprobamos si los datos forman parte de un cuadro de texto o de un botón
+
+  //Recogemos los datos que forman parte del fieldset
   let valor = parseInt(formCalc.value)
   if (isNaN(valor) || valor < 0 || valor > 100) {
     msg.innerText = "Valores entre 0 y 100"
     return 0
   }
-  console.log(valor)
+  valor *= 0.01
+
+  //Reestablecemos los colores del grupo de botones y cambiamos el seleccionado
+  if (formCalc.querySelector("input[type='button']")) {
+    console.log(document.querySelector('input[type="button"].btn_Tip_selected'))
+    if (document.querySelector("input.btn_Tip_selected")) {
+      document.querySelector("input.btn_Tip_selected").className = "btn_Tip"
+    }
+    formCalc.className = "btn_Tip_selected"
+  }
+  //Calculamos el tipo de Interés/persona
+  const facturar = parseFloat(calc_form.form_bill.value)
+  if (isNaN(facturar)) {
+    msg.innerText = " Facturación incorrecta"
+  }
+  const tip = (facturar * valor) / usuario
+  const total = facturar / usuario + tip
+  document.getElementById("monitor_Amount_Result").innerText = tip.toFixed(2)
+  document.getElementById("monitor_Total_Result").innerText = total.toFixed(2)
 }
