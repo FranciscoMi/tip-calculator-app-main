659725e86dc75e47a7858566228c3a7e19a390b3
FranciscoMi <franlope@gmail.com>
2021-07-29 10:39:20 +0200

    Añadido modificación en tiempo real

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ function calcform(formCalc) {
   msg.forEach((clase) => {
     clase.innerText = ""
   })
-
   //Comprobamos que se haya escrito al menos, un usuario
   msg = document.getElementById("form_msg_user")
   const usuario = document.calc_form.form_People.value
@@ -26,26 +25,34 @@ function calcform(formCalc) {
     return 0
   }
 
-  //Reestablecemos los colores del grupo de botones y cambiamos el seleccionado
-  if (document.querySelector("input.btn_Tip_selected")) {
-    document.querySelector("input.btn_Tip_selected").className = "btn_Tip"
-  }
-  if (formCalc.type === "button") {
-    formCalc.className = "btn_Tip_selected"
-  }
-
-  //Recogemos los datos que forman parte del campo Facturar
+  //Recogemos los datos que forman parte del campo Facturar según el cuadro que estemos tecleando
   msg = document.getElementById("form_msg_Tip")
   let valor = parseInt(formCalc.value)
-  if (isNaN(valor)) {
-    msg.innerText = "Valor no entero"
-    return 0
-  } else if (valor < 0 || valor > 100) {
-    msg.innerText = "Valores (0 - 100)"
-    return 0
+  let input_txt = formCalc.name
+  switch (input_txt) {
+    case "tip":
+      //Reestablecemos los colores del grupo de botones y cambiamos el seleccionado
+      if (document.querySelector("input.btn_Tip_selected")) {
+        document.querySelector("input.btn_Tip_selected").className = "btn_Tip"
+      }
+      if (formCalc.type === "button") {
+        formCalc.className = "btn_Tip_selected"
+        document.getElementById("form_custom").value = ""
+      }
+
+      if (isNaN(valor)) {
+        msg.innerText = "Valor no entero"
+        return 0
+      } else if (valor < 0 || valor > 100) {
+        msg.innerText = "Valores (0 - 100)"
+        return 0
+      }
   }
   valor *= 0.01
+  showValue(valor, usuario)
+}
 
+function showValue(valor, usuario) {
   //Calculamos el tipo de Interés/persona
   const facturar = parseFloat(calc_form.form_bill.value)
   if (isNaN(facturar)) {
